036e1bcda7e0985ebc88860698b550818bae3304
matt-macs <matt.almeidac@gmail.com>
2026-08-23 02:49:22 -0300

    feat: :sparkle: add mais uma tab e muda o estilo de interagir

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,4 +1,5 @@
 import { theme } from "@/src/styles/theme";
+import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
@@ -7,24 +8,44 @@ export default function TabsLayout() {
 			screenOptions={{
 				headerShown: true,
 				tabBarActiveTintColor: theme.colors.primary,
+				tabBarActiveBackgroundColor: "#1B4BA080",
 				tabBarInactiveTintColor: theme.colors.textSecondary,
 				tabBarStyle: {
 					backgroundColor: theme.colors.background,
-					borderTopWidth: 0,
+					borderBlockColor: "white",
+					borderTopWidth: 1,
 				},
 			}}
 		>
 			<Tabs.Screen
-				name="index"
+				name="routines"
 				options={{
 					title: "Rotinas",
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons
+							name="list-outline"
+							size={size}
+							color={color}
+						/>
+					),
 				}}
 			/>
-
 			<Tabs.Screen
-				name="create"
+				name="index"
 				options={{
-					title: "Nova Rotina",
+					title: "início",
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons name="home" size={size} color={color} />
+					),
+				}}
+			/>
+			<Tabs.Screen
+				name="options"
+				options={{
+					title: "Opções",
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons name="settings" size={size} color={color} />
+					),
 				}}
 			/>
 		</Tabs>
