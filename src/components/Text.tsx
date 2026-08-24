39e02133658ca46951e04b0810fbdfa005562301
matt-macs <matt.almeidac@gmail.com>
2026-08-24 17:24:23 -0300

    feat: :construction: tentando entender como estilizar texto padrao

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,49 +1,23 @@
-// src/components/Text.tsx
-
 import React from "react";
 import { Text as RNText, TextProps as RNTextProps } from "react-native";
 import { theme, TypographyVariant } from "../styles/theme";
 
 interface TextProps extends RNTextProps {
 	variant?: TypographyVariant;
-	color?: keyof typeof theme.colors;
-	/** Permite alternar a variação da fonte para regular, medium ou italic */
-	fontStyle?: "regular" | "medium" | "italic" | "bold";
+	color?: keyof typeof theme.colors | string;
 }
 
 export function Text({
 	variant = "p",
-	color = "text",
-	fontStyle,
+	color = theme.colors.text,
 	style,
 	children,
 	...rest
 }: TextProps) {
 	const typographyStyle = theme.typography[variant];
-	const textColor = theme.colors[color];
-
-	// Se o estilo de fonte for especificado, identificamos se a variante atual usa HostGrotesk ou Quicksand
-	let customFontFamily: string | undefined = undefined;
-
-	if (fontStyle) {
-		const isHostGrotesk =
-			typographyStyle.fontFamily.includes("HostGrotesk");
-		const fontGroup = isHostGrotesk
-			? theme.fonts.hostGrotesk
-			: theme.fonts.quicksand;
-		customFontFamily = fontGroup[fontStyle];
-	}
 
 	return (
-		<RNText
-			style={[
-				typographyStyle,
-				{ color: textColor },
-				customFontFamily ? { fontFamily: customFontFamily } : null,
-				style,
-			]}
-			{...rest}
-		>
+		<RNText style={[typographyStyle, { color }, style]} {...rest}>
 			{children}
 		</RNText>
 	);
