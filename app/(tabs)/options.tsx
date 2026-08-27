ab737b97d86ea604690d2606f41dedc323d92391
matt-macs <matt.almeidac@gmail.com>
2026-08-27 00:02:22 -0300

    feat: :construction: add elementos da pag

--- app/(tabs)/options.tsx
+++ app/(tabs)/options.tsx
@@ -1,21 +1,77 @@
 import { theme, txtStyles } from "@/src/styles/theme";
-import { ScrollView, Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function Options() {
-	return (
-		<ScrollView
-			contentContainerStyle={{
-				backgroundColor: theme.colors.background,
-				padding: theme.spacing.d,
-				flex: 1,
-				gap: theme.spacing.e,
-			}}
-		>
-			<View>
-				<Text style={txtStyles.h1}>Configurações</Text>
+    return (
+        <ScrollView
+            contentContainerStyle={{
+                backgroundColor: theme.colors.background,
+                padding: theme.spacing.d,
+                flex: 1,
+                gap: theme.spacing.e,
+            }}
+        >
+            <View>
+                <Text style={txtStyles.h1}>Menu</Text>
+            </View>
+            <View style={styles.user}>
+                <Image src="" width={140} />
+                <View style={styles.userInfo}>
+                    <Text style={[txtStyles.p, styles.userName]}>
+                        Matheus de Almeida Campos
+                    </Text>
+                    <Text style={txtStyles.sub}>Entrou em: 05/08/2026</Text>
+                </View>
+            </View>
+            <View style={styles.optionsContainer}>
+                <View>
+                    <Text style={txtStyles.h2}>Opções</Text>
+                    <Link href="/" style={txtStyles.p}>
+                        Editar Perfil
+                    </Link>
+                    <Link href="/" style={txtStyles.p}>
+                        Preferências
+                    </Link>
+                    <Link href="/" style={txtStyles.p}>
+                        Gerenciar Ações
+                    </Link>
+                </View>
 
-				<Text style={txtStyles.p}>omg hiii</Text>
-			</View>
-		</ScrollView>
-	);
+                <Link href="/" style={txtStyles.p}>
+                    Excluir Conta
+                </Link>
+                <Link href="/" style={txtStyles.p}>
+                    Sobre
+                </Link>
+                <Text style={txtStyles.sub}>Versão 0.0.8 alpha</Text>
+            </View>
+        </ScrollView>
+    );
 }
+
+const styles = StyleSheet.create({
+    user: {
+        gap: theme.spacing.d,
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    userPic: {
+        width: 128,
+        backgroundColor: theme.colors.primary,
+    },
+    userName: {
+        fontSize: 20,
+        fontWeight: 500,
+    },
+    userInfo: {
+        gap: theme.spacing.b,
+    },
+    optionsContainer: {
+        gap: theme.spacing.e,
+    },
+    about: {
+        flex: 1,
+        flexDirection: "column",
+    },
+});
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,101 +1,113 @@
 export const theme = {
-	colors: {
-		background: "#0E182C",
+    colors: {
+        background: "#0E182C",
 
-		card: "#1b4ca019",
-		cardShadow: "#0E182C6b",
+        card: "#1b4ca019",
+        cardShadow: "#0E182C6b",
 
-		primary: "#E0E06E",
-		secondary: "#1B4BA0",
+        primary: "#E0E06E",
+        secondary: "#1B4BA0",
 
-		text: "#FBF5E9",
-		textSecondary: "#2828b6",
-		muted: "#A8A8B3",
-	},
+        text: "#FBF5E9",
+        textSecondary: "#2828b6",
+        muted: "#A8A8B3",
+    },
 
-	gradients: {
-		bgGradient: ["#0E182C", "#2E5092"] as const,
-	},
+    gradients: {
+        bgGradient: ["#0E182C", "#2E5092"] as const,
+    },
 
-	spacing: { a: 4, b: 8, c: 16, d: 24, e: 32 },
+    spacing: { a: 4, b: 8, c: 16, d: 24, e: 32 },
 
-	borderRadius: { s: 4, m: 8, l: 16 },
+    borderRadius: { s: 4, m: 8, l: 16 },
 
-	iconSize: { ss: 16, s: 20, m: 24, l: 32 },
+    iconSize: { ss: 16, s: 20, m: 24, l: 32 },
 
-	fonts: {
-		hostGrotesk: {
-			italic: "HostGrotesk-Italic",
-			regular: "HostGrotesk-Regular",
-			medium: "HostGrotesk-Medium",
-			bold: "HostGrotesk-Bold",
-		},
-		quicksand: {
-			italic: "Quicksand-Light",
-			regular: "Quicksand-Regular",
-			medium: "Quicksand-Medium",
-			bold: "Quicksand-Bold",
-		},
-	},
+    fonts: {
+        hostGrotesk: {
+            italic: "HostGrotesk-Italic",
+            regular: "HostGrotesk-Regular",
+            medium: "HostGrotesk-Medium",
+            bold: "HostGrotesk-Bold",
+        },
+        quicksand: {
+            italic: "Quicksand-Light",
+            regular: "Quicksand-Regular",
+            medium: "Quicksand-Medium",
+            bold: "Quicksand-Bold",
+        },
+    },
 };
 
 export const txtStyles = {
-	title: {
-		fontFamily: "HostGrotesk-Bold",
-		fontSize: 36,
-		lineHeight: 44,
-		color: theme.colors.text,
-	},
-	h1: {
-		fontFamily: "HostGrotesk-Bold",
-		fontSize: 42,
-		lineHeight: 50,
-		color: theme.colors.text,
-	},
-	h2: {
-		fontFamily: "HostGrotesk-Bold",
-		fontSize: 36,
-		lineHeight: 44,
-		color: theme.colors.text,
-	},
-	h3: {
-		fontFamily: "HostGrotesk-Bold",
-		fontSize: 30,
-		lineHeight: 38,
-		color: theme.colors.text,
-	},
+    title: {
+        fontFamily: "HostGrotesk-Bold",
+        fontSize: 36,
+        lineHeight: 44,
+        color: theme.colors.text,
+    },
+    h1: {
+        fontFamily: "HostGrotesk-Bold",
+        fontSize: 42,
+        lineHeight: 50,
+        color: theme.colors.text,
+    },
+    h2: {
+        fontFamily: "HostGrotesk-Bold",
+        fontSize: 36,
+        lineHeight: 44,
+        color: theme.colors.text,
+    },
+    h3: {
+        fontFamily: "HostGrotesk-Bold",
+        fontSize: 30,
+        lineHeight: 38,
+        color: theme.colors.text,
+    },
 
-	// --- Quicksand ---
-	h4: {
-		fontFamily: "Quicksand-Bold",
-		fontSize: 24,
-		lineHeight: 30,
-		color: theme.colors.text,
-	},
-	h5: {
-		fontFamily: "Quicksand-Bold",
-		fontSize: 20,
-		lineHeight: 26,
-		color: theme.colors.text,
-	},
-	h6: {
-		fontFamily: "Quicksand-Regular",
-		fontSize: 14,
-		lineHeight: 18,
-		color: theme.colors.text,
-	},
-	p: {
-		fontFamily: "Quicksand-Medium",
-		fontSize: 16,
-		lineHeight: 22,
-		color: theme.colors.text,
-	},
-	p2: {
-		fontFamily: "Quicksand-Bold",
-		fontSize: 16,
-		lineHeight: 22,
-		color: theme.colors.text,
-	},
+    // --- Quicksand ---
+    h4: {
+        fontFamily: "Quicksand-Bold",
+        fontSize: 24,
+        lineHeight: 30,
+        color: theme.colors.text,
+    },
+    h5: {
+        fontFamily: "Quicksand-Bold",
+        fontSize: 20,
+        lineHeight: 26,
+        color: theme.colors.text,
+    },
+    h6: {
+        fontFamily: "Quicksand-Regular",
+        fontSize: 14,
+        lineHeight: 18,
+        color: theme.colors.text,
+    },
+    p: {
+        fontFamily: "Quicksand-Medium",
+        fontSize: 16,
+        lineHeight: 22,
+        color: theme.colors.text,
+    },
+    p1: {
+        fontFamily: "Quicksand",
+        fontSize: 16,
+        lineHeight: 22,
+        color: theme.colors.text,
+    },
+    p2: {
+        fontFamily: "Quicksand-Bold",
+        fontSize: 16,
+        lineHeight: 22,
+        color: theme.colors.text,
+    },
+    sub: {
+        fontFamily: "Quicksand-Light",
+        fontSize: 14,
+        // lineHeight: 20,
+        color: theme.colors.text,
+    },
 };
 
 // export type Theme = typeof theme;
