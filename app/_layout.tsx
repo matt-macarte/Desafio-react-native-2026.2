570d47c5209216549db4632f27e96c6534784c4a
matt-macs <matt.almeidac@gmail.com>
2026-08-28 17:03:50 -0300

    feat: :hammer: add todas fontes pra carregar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,9 +10,20 @@ export default function RootLayout() {
 	const [loaded, error] = useFonts({
 		"HostGrotesk-Regular": require("../assets/fonts/HostGrotesk-Regular.ttf"),
 		"HostGrotesk-Bold": require("../assets/fonts/HostGrotesk-Bold.ttf"),
+		"HostGrotesk-BoldItalic": require("../assets/fonts/HostGrotesk-BoldItalic.ttf"),
+		"HostGrotesk-ExtraBold": require("../assets/fonts/HostGrotesk-ExtraBold.ttf"),
+		"HostGrotesk-Italic": require("../assets/fonts/HostGrotesk-Italic.ttf"),
+		"HostGrotesk-Light": require("../assets/fonts/HostGrotesk-Light.ttf"),
+		"HostGrotesk-LightItalic": require("../assets/fonts/HostGrotesk-LightItalic.ttf"),
+		"HostGrotesk-Medium": require("../assets/fonts/HostGrotesk-Medium.ttf"),
+		"HostGrotesk-SemiBold": require("../assets/fonts/HostGrotesk-SemiBold.ttf"),
+		"HostGrotesk-MediumItalic": require("../assets/fonts/HostGrotesk-MediumItalic.ttf"),
+		// quicksand
 		"Quicksand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
 		"Quicksand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
 		"Quicksand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+		"Quicksand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+		"Quicksand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
 	});
 
 	useEffect(() => {
